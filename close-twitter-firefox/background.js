2ddd429cee75a350c6d0ba299b5cc414a091fdf2
Alameen688 <ogundiran12@gmail.com>
2019-03-26 19:38:10 +0100

    bg(tab-close): fix :bug: invalid format url preventing matching of tab urls  - remove functionality code from catch block  - add condition for scenario where message.urls is undefined,   which refers to the event where message is from default config and not   user registered url

--- close-twitter-firefox/background.js
+++ close-twitter-firefox/background.js
@@ -15,30 +15,35 @@ async function registerWebsite(message, sender) {
     let minute = 60000;
 
     try {
-        let urls = message.urls;
-        let formattedUrls = [];
 
-        for (let url of urls) {
-            formattedUrls.push(`"*://*.${url}/*"`)
-        }
-        minute = message.minute * 60 * 1000;
+        if (message.urls) {
+            minute = parseInt(message.minute) * 60 * 1000;
+        
+            let urls = message.urls;
+            let formattedUrls = [];
 
-        if (registered) {
-            registered.unregister();
-        }
+            for (let url of urls) {
+                formattedUrls.push(`*://*.${url}/*`);
+            }
 
-        registered = await browser.contentScripts.register({
-            matches: formattedUrls,
-            js: [{file : "close-twitter.js"}],
-            runAt: "document_idle",
-        });
 
+            if (registered) {
+                registered.unregister();
+            }
+
+            registered = await browser.contentScripts.register({
+                matches: formattedUrls,
+                js: [{file : "close-twitter.js"}],
+                runAt: "document_idle",
+            });
+
+        } 
+        
         await sleep(minute);
         browser.tabs.remove(sender.tab.id);
     }
     catch (e) {
-        await sleep(minute);
-        browser.tabs.remove(sender.tab.id);
+        console.log(e);
     }
 
     // let urls = message.urls;
@@ -65,9 +70,3 @@ async function registerWebsite(message, sender) {
 
 browser.runtime.onMessage.addListener(registerWebsite);
 // browser.runtime.onMessage.addListener(closeTab);
-
-
-
-
-
-
